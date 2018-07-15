03eca2c152b123b040963b7aefde7949a56c856e
Gabi Stanciu <gabriel.stanciu@updivision.com>
2018-07-15 16:08:10 +0300

    added .babelrc config file

--- dist/vue2-poll.js
+++ dist/vue2-poll.js
@@ -11,41 +11,41 @@
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-/******/
+
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-/******/
+
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-/******/
+
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
+
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/
+
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-/******/
-/******/
+
+
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-/******/
+
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-/******/
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-/******/
+
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -64,7 +64,23 @@ return /******/ (function(modules) { // webpackBootstrap
 	  /* cssModules */
 	  null
 	)
-	
+	Component.options.__file = "C:\\Users\\miha\\Desktop\\vue2-poll\\src\\Poll.vue"
+	if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
+	if (Component.options.functional) {console.error("[vue-loader] Poll.vue: functional components are not supported with templates, they should use render functions.")}
+
+	/* hot reload */
+	if (false) {(function () {
+	  var hotAPI = require("vue-hot-reload-api")
+	  hotAPI.install(require("vue"), false)
+	  if (!hotAPI.compatible) return
+	  module.hot.accept()
+	  if (!module.hot.data) {
+	    hotAPI.createRecord("data-v-bd8d5332", Component.options)
+	  } else {
+	    hotAPI.reload("data-v-bd8d5332", Component.options)
+	  }
+	})()}
+
 	module.exports = Component.exports
 
 
@@ -74,7 +90,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	// this module is a runtime utility for cleaner component module output and will
 	// be included in the final webpack user bundle
-	
+
 	module.exports = function normalizeComponent (
 	  rawScriptExports,
 	  compiledTemplate,
@@ -83,30 +99,30 @@ return /******/ (function(modules) { // webpackBootstrap
 	) {
 	  var esModule
 	  var scriptExports = rawScriptExports = rawScriptExports || {}
-	
+
 	  // ES6 modules interop
 	  var type = typeof rawScriptExports.default
 	  if (type === 'object' || type === 'function') {
 	    esModule = rawScriptExports
 	    scriptExports = rawScriptExports.default
 	  }
-	
+
 	  // Vue.extend constructor export interop
 	  var options = typeof scriptExports === 'function'
 	    ? scriptExports.options
 	    : scriptExports
-	
+
 	  // render functions
 	  if (compiledTemplate) {
 	    options.render = compiledTemplate.render
 	    options.staticRenderFns = compiledTemplate.staticRenderFns
 	  }
-	
+
 	  // scopedId
 	  if (scopeId) {
 	    options._scopeId = scopeId
 	  }
-	
+
 	  // inject cssModules
 	  if (cssModules) {
 	    var computed = Object.create(options.computed || null)
@@ -116,7 +132,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    })
 	    options.computed = computed
 	  }
-	
+
 	  return {
 	    esModule: esModule,
 	    exports: scriptExports,
@@ -133,7 +149,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	//
 	//
 	//
-	
+
 	export default {
 	    data() {
 	        return {
@@ -144,11 +160,18 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 3 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
 	  return _c('div', [_c('strong', [_vm._v(" " + _vm._s(_vm.message))])])
 	},staticRenderFns: []}
+	module.exports.render._withStripped = true
+	if (false) {
+	  module.hot.accept()
+	  if (module.hot.data) {
+	     require("vue-hot-reload-api").rerender("data-v-bd8d5332", module.exports)
+	  }
+	}
 
 /***/ }
 /******/ ])
