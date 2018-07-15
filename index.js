17b1b32b81919c033d1bb833ad0fc3780e0f05a2
Gabi Stanciu <gabriel.stanciu@updivision.com>
2018-07-15 15:20:39 +0300

    Added .gitignore

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 export default {
     install(Vue, options) {
         // We call Vue.mixin() here to inject functionality into all components.
-          Vue.mixin({
-          // Anything added to a mixin will be injected into all components.
-          // In this case, the mounted() method runs when the component is added to the DOM.
-          mounted() {
-            console.log('Mounted!');
-          }
+        Vue.mixin({
+            // Anything added to a mixin will be injected into all components.
+            // In this case, the mounted() method runs when the component is added to the DOM.
+            mounted() {
+                console.log('Mounted!');
+            }
         });
-      }
+    }
 };
