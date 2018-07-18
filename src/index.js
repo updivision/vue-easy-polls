1716b4666778bc59fe534508f2f690e0f6018854
Gabi Stanciu <gabriel.stanciu@updivision.com>
2018-07-18 21:22:55 +0300

    created index.html and fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import PollView from './PollView.vue';
 import PollCreator from './PollCreator.vue'
 
-const Poll = {
-  PollView,
-  PollCreator
-}
-
-export default Poll 
-
-export {
-  Poll,
-  PollView,
-  PollCreator
-}
+module.exports = {
+  install: function (Vue, options) {
+    Vue.component('poll-view', PollView);
+    Vue.component('poll-creator', PollCreator);
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = [
     entry: path.resolve(__dirname + '/src/index.js'),
     output: {
       filename: 'vue2-poll.min.js',
-      libraryTarget: 'umd',
+      libraryTarget: 'window',
       library: 'Vue2Poll',
     }
   })
